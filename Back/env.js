69439fd0c92ed58c8b2c9beb7e2bb39ef4d5c666
LuisHackABoss <luis.saavedra.vazquez@hackaboss.com>
2024-06-12 11:57:08 +0200

    Añadir el Back

---
+++ Back/env.js
@@ -0,0 +1,33 @@
+// Cargamos las variables de entorno
+import 'dotenv/config';
+
+// Importar las variables de entorno del .env
+const {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+  MYSQL_PORT,
+  PORT,
+  SECRET,
+  UPLOADS_DIR,
+  SMTP_SERVICE,
+  SMTP_PORT,
+  SMTP_USER,
+  SMTP_PASS,
+} = process.env;
+
+export {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+  MYSQL_PORT,
+  PORT,
+  SECRET,
+  UPLOADS_DIR,
+  SMTP_SERVICE,
+  SMTP_PORT,
+  SMTP_USER,
+  SMTP_PASS,
+};
